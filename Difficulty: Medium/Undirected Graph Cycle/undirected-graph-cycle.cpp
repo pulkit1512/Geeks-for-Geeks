b6e25f4291b7b8b11ca2af980bd22f8fa5c15a1f
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-08-07 15:56:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,44 +1,38 @@
 class Solution {
-public:
-    bool bfs(int start, vector<vector<int>>& adj, vector<bool>& visited) {
-        queue<pair<int, int>> q;
-        q.push({start, -1});
-        visited[start] = true;
-
-        while (!q.empty()) {
-            int node = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-
-            for (int neighbor : adj[node]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    q.push({neighbor, node});
-                } else if (neighbor != parent) {
-                    return true; // Cycle detected
-                }
-            }
-        }
-
-        return false;
-    }
-
-    bool isCycle(int V, vector<vector<int>>& edges) {
-        // Build adjacency list
-        vector<vector<int>> adj(V);
-        for (auto& edge : edges) {
-            int u = edge[0], v = edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        vector<bool> visited(V, false);
-        for (int i = 0; i < V; ++i) {
-            if (!visited[i]) {
-                if (bfs(i, adj, visited)) return true;
-            }
-        }
-
-        return false;
-    }
+	public:
+	
+	bool dfs(vector<vector<int>> &adj, vector<bool>&visited, int parent, int node) {
+		visited[node] = 1;
+		for (int i = 0; i<adj[node].size(); i++) {
+			if (parent == adj[node][i])
+				continue;
+			if (visited[adj[node][i]])
+				return 1;
+			if (dfs(adj, visited, node, adj[node][i])) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+	bool isCycle(int V, vector<vector<int>> & edges) {
+		// Code here
+		vector<vector<int>> adj(V);
+		for (int i = 0; i<edges.size(); i++) {
+			int u = edges[i][0];
+			int v = edges[i][1];
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		// handle case for the broken graph
+		vector<bool>visited(V, 0);
+		for (int i = 0; i<V; i++) {
+			if (!visited[i]) {
+				if (dfs(adj, visited, -1, i)) {
+					return 1;
+				}
+			}
+		}
+		return 0;
+		
+	}
 };
