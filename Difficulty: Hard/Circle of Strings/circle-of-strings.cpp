68ee120c038b185b3ac0de4c09d8a734a3ac7a5c
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-08-29 20:03:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Circle of Strings/circle-of-strings.cpp
+++ Difficulty: Hard/Circle of Strings/circle-of-strings.cpp
@@ -1,21 +1,21 @@
 class Solution {
   public:
-  void dfs(int node,vector<int>adj[],vector<bool>&visited){
-      visited[node]=1;
-      for(int i=0;i<adj[node].size();i++){
-          if(!visited[adj[node][i]]){
-              dfs(adj[node][i],adj,visited);
-          }
-      }
-  }
+    void dfs(vector<vector<int>>&adj,vector<bool>&visited,int node){
+        visited[node]=1;
+        
+        for(int i=0;i<adj[node].size();i++){
+            if(!visited[adj[node][i]]){
+                dfs(adj,visited,adj[node][i]);
+            }
+        }
+    }
     bool isCircle(vector<string> &arr) {
         // code here
         int n=arr.size();
-        vector<int>adj[26];
         vector<int>indeg(26,0);
         vector<int>outdeg(26,0);
-        
-        for(int i=0;i<n;i++){
+        vector<vector<int>>adj(26);
+        for(int i=0;i<arr.size();i++){
             int u=arr[i][0]-'a';
             int v=arr[i][arr[i].size()-1]-'a';
             adj[u].push_back(v);
@@ -24,18 +24,23 @@ class Solution {
         }
         
         for(int i=0;i<26;i++){
-            if(indeg[i]!=outdeg[i]) return 0;
+            if(indeg[i]!=outdeg[i]){
+                return false;
+            }
         }
         
         vector<bool>visited(26,0);
         
-        int node=arr[0][0]-'a';
-        
-        dfs(node,adj,visited);
-        
-        for(int i=0;i<26;i++){
-            if(indeg[i]&&!visited[i]) return 0;
+        for(int  i=0;i<26;i++){
+            if(indeg[i]){
+                dfs(adj,visited,i);
+                break;
+            }
         }
-        return 1;
+        for(int i=0;i<26;i++){
+            if(indeg[i]&&!visited[i]) return false;
+        }
+        
+        return true;
     }
 };
