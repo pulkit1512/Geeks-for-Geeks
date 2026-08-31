d62f96e810a6540c7842fd61ecb17cb0d29d7ec4
Pulkit Sharma <u24ai086@aid.svnit.ac.in>
2026-08-31 11:58:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,35 +1,56 @@
 class Solution {
   public:
+     int findparent(int u,vector<int>&parent){
+         if(u==parent[u]) return u;
+         
+         return parent[u]=findparent(parent[u],parent);
+     }
+     
+     void unionrank(int u,int v,vector<int>&rank,vector<int>&parent){
+         int pu=findparent(u,parent);
+         int pv=findparent(v,parent);
+         
+         if(rank[pu]>rank[pv]) parent[pv]=pu;
+         else if(rank[pu]<rank[pv]) parent[pu]=pv;
+         else{
+             parent[pv]=pu;
+             rank[pu]++;
+         }
+     }
+    
     int spanningTree(int V, vector<vector<int>>& edges) {
         // code here
-        vector<vector<pair<int,int>>>adj(V);
+        vector<int>parent(V);
+        vector<int>rank(V);
+        for(int i=0;i<V;i++){
+            parent[i]=i;
+        }
         
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
         for(int i=0;i<edges.size();i++){
             int u=edges[i][0];
             int v=edges[i][1];
-            int w=edges[i][2];
-            adj[u].push_back({v,w});
-            adj[v].push_back({u,w});
+            int wt=edges[i][2];
+            
+            pq.push({wt,{u,v}});
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,0});
-        vector<bool>visited(V,0);
-        int sum=0;
-        
+        int ans=0;
         while(!pq.empty()){
-            auto [w,node]=pq.top();
+            auto [wt, p] = pq.top();
+           
+            int u = p.first;
+            int v = p.second;
             pq.pop();
-            if(visited[node]) continue;
-            visited[node]=1;
-            sum+=w;
-            for(int i=0;i<adj[node].size();i++){
-                auto [v,weight]=adj[node][i];
-                if(!visited[v]){
-                    pq.push({weight,v});
-                }
+            if(findparent(u,parent)!=findparent(v,parent)){
+                ans+=wt;
+                unionrank(u,v,rank,parent);
+                
             }
+            
+            
+            
+            
         }
-        
-        return sum;
+        return ans;
     }
 };
